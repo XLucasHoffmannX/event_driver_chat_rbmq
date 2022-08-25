1b4096d82610ab88a6d30ca1c5ddec561e60aa44
XLucasHoffmannX <henriquelucashoffmann@gmail.com>
2022-08-24 21:26:46 -0300

    hotfix's realizados

--- web-client/src/resources/components/ChatComponents/ChatSend/index.tsx
+++ web-client/src/resources/components/ChatComponents/ChatSend/index.tsx
@@ -3,10 +3,11 @@ import { RiSendPlane2Line } from 'react-icons/ri';
 import { HiOutlineEmojiHappy } from 'react-icons/hi';
 import changeInputRecursive from '../../../../app/helpers/ChangeInputRecursive';
 import { v4 as uuidv4 } from 'uuid';
+import ChatEmojiArea from '../ChatEmojiArea';
 
 export default function ChatSend({ socket, username, room, messageList, setMessageList }: any) {
-    const [emojiPicker, setEmojiPicker] = React.useState(false);
     /* eslint-disable */
+    const [emojiArea, setEmojiArea] = React.useState(false);
     const [emojiText, setEmojiText] = React.useState('');
     const [currentMessage, setCurrentMessage] = React.useState<any>({
         message: ''
@@ -30,40 +31,47 @@ export default function ChatSend({ socket, username, room, messageList, setMessa
             await socket.emit("send_message", messageData);
             setMessageList((list: any) => [...list, messageData]);
             setCurrentMessage({ message: '' });
+            console.log(messageData);
         }
     }
 
     React.useEffect(() => {
         socket.on("receive_message", (data: any) => {
             setMessageList((list: any) => [...list, data]);
-            console.log(messageList)
         });
     }, [socket]);
 
     return (
-        <div className='chat_send'>
-            <div className='chat_send_events'>
-                <div className='emoji_picker_chat'>
-                    <HiOutlineEmojiHappy
-                        onClick={() => emojiPicker ? setEmojiPicker(false) : setEmojiPicker(true)}
-                    />
-                </div>
-                <div className='message_input_display'>
-                    <input type="text" className='input_message' placeholder='Digite sua mensagem'
-                        value={currentMessage.message}
-                        name='message'
-                        onChange={changeInput}
-                        onKeyPress={(event) => {
-                            event.key === "Enter" && sendMessage();
-                        }}
-                    />
-                </div>
-                <div className='send_picker'>
-                    <RiSendPlane2Line
-                        onClick={sendMessage}
-                    />
+        <>
+            <ChatEmojiArea 
+                emojiArea={emojiArea}
+                currentMessage={currentMessage}
+                setCurrentMessage={setCurrentMessage}
+            />
+            <div className='chat_send'>
+                <div className='chat_send_events'>
+                    <div className='emoji_picker_chat'>
+                        <HiOutlineEmojiHappy
+                            onClick={() => emojiArea ? setEmojiArea(false) : setEmojiArea(true)}
+                        />
+                    </div>
+                    <div className='message_input_display'>
+                        <input type="text" className='input_message' placeholder='Digite sua mensagem'
+                            value={currentMessage.message}
+                            name='message'
+                            onChange={changeInput}
+                            onKeyPress={(event) => {
+                                event.key === "Enter" && sendMessage();
+                            }}
+                        />
+                    </div>
+                    <div className='send_picker'>
+                        <RiSendPlane2Line
+                            onClick={sendMessage}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
